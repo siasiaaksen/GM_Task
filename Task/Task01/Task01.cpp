db68f0a2fdcc850058caa2a0d8272c1e9d25023b
siasiaaksen <jj818181@naver.com>
2024-09-23 16:31:24 +0900

    이재영 : task1

---
+++ Task/Task01/Task01.cpp
@@ -0,0 +1,45 @@
+﻿// 001_NumberCount.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+// 음수 안함
+// 0 이외의 양수의 자릿수를 세자.
+int NumberCount(int _Value)
+{
+    // /(나누기를 최대한) 연산을 사용하세요.
+    int Result = 0;
+    while (_Value)
+    {
+        _Value /= 10;
+        Result += 1;
+    }
+
+    return Result;
+}
+
+// 해도 되고 안해도 됩니다.
+// 내 기준으로 지금
+// _Value 를 넣어주면 Buffer에 
+// 숫자를 문자열로 만드는 함수.
+void NumberToString(char* Buffer, int BufferSize, int _Value)
+{
+    return;
+}
+
+int main()
+{
+    // 5
+    int Result0 = NumberCount(12358);
+    // 4
+    int Result1 = NumberCount(5258);
+    // 7
+    int Result2 = NumberCount(5258111);
+
+    // 0은 영
+
+    char Buffer[100] = { 0 };
+
+    NumberToString(Buffer, 100, 3712);
+    // Buffer == "3712"
+}
