e1e9d4ecff44d682808ee4e641720d71bb2f8759
siasiaaksen <jj818181@naver.com>
2024-09-23 15:16:39 +0900

    이재영 : task01

---
+++ Task/Task/Task.cpp
@@ -0,0 +1,32 @@
+﻿// 000_StringCount.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+// 지금만들라고 하는 함수들은
+// strlen
+
+int StringCount(const char* const _Str)
+{
+    int Count = 0;
+
+    while (_Str[Count])
+    {
+        Count += 1;
+    }
+
+    return Count;
+}
+
+int main()
+{
+    // [B][B][B][B][0]
+
+    //int Count = strlen("BBBB");
+
+    int Count = StringCount("BBBB");
+
+
+
+    int a = 0;
+}
